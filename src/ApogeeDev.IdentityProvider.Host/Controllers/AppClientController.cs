242f41d795eec6c52fb32bedf782e0211b4aee0a
Ajay Sharma <ajay.sharma1984@gmail.com>
2025-11-12 21:54:55 -0500

    initial idp API

---
+++ src/ApogeeDev.IdentityProvider.Host/Controllers/AppClientController.cs
@@ -0,0 +1,46 @@
+using ApogeeDev.IdentityProvider.Host.Operations.RequestHandlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApogeeDev.IdentityProvider.Host.Controllers;
+
+[ApiController]
+[Route("api/app-client")]
+public class AppClientController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public AppClientController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public async Task<IActionResult> Client()
+    {
+        var response = await mediator.Send(new AppClientListRequest());
+
+        return Ok(response);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<IActionResult> ClientEdit(string id, [FromBody] AppClientData model)
+    {
+        return await Task.FromResult(NoContent());
+    }
+
+    [HttpPost]
+    [Route("")]
+    public IActionResult ClientAdd([FromBody] AppClientData model)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route("{id}/client-secret")]
+    public IActionResult CreateClientSecret(string id)
+    {
+        return Ok();
+    }
+}
